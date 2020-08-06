c3e6437846ae453eacf535e10f6f03260f668b55
Vinicius Martins <contato@vinimartdev.com>
2020-08-06 12:04:09 -0300

    Método POST

--- api/cliente.js
+++ api/cliente.js
@@ -1,4 +1,23 @@
+// GET
 const listarClientes = async () => {
     const response = await fetch('http://localhost:4000/clientes');
     return response.json();
+};
+
+// POST
+const cadastrarClientes = async (nome, cpf) => {
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf,
+    });
+
+    const response = await fetch('http://localhost:4000/clientes/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: json,
+    });
+
+    return response.body
 };
